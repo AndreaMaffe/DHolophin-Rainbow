1830812ede87fa8ed57f6e0ba0809019ac276c32
Gianmarco Dello Preite Castro <gianmarco.dello@mail.polimi.it>
2018-12-06 12:28:09 +0100

    added json event

---
+++ Assets/Scripts/JsonEvent.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventJson
+{
+
+
+    public class Event
+    {
+        public string type { get; set; }
+        public string id { get; set; }
+        public string val { get; set; }
+        public string dur { get; set; }
+        public string SemVal { get; set; }
+    }
+
+    public class RootObject
+    {
+        public Event Event { get; set; }
+    }
+}
+
