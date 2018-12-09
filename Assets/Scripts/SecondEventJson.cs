4ceed81924a8d0bef1bb3c2f69da8062d4efb4e2
gianmarcodpc <gianmarco.dello@mail.polimi.it>
2018-12-09 15:16:29 +0100

    Added classes for json messages

---
+++ Assets/Scripts/SecondEventJson.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SecondEventJson : MonoBehaviour
+{
+
+    public class SecondEvent
+    {
+        public string typ { get; set; }
+        public string val { get; set; }
+        public int act { get; set; }
+        public int dur { get; set; }
+    }
+
+    public class RootObject
+    {
+        public SecondEvent Event { get; set; }
+    }
+}
