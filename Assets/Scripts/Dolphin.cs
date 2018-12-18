8dafa165bfb6f0423beb59d2394788c6d792bbcf
AndreaMaffe <andrea1.mafessoni@mail.polimi.it>
2018-12-18 14:50:15 +0100

    Dolphin small animation

---
+++ Assets/Scripts/Dolphin.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dolphin : MonoBehaviour
+{
+    private SpriteRenderer spriteRenderer;
+    private Sprite happySprite, defaultSprite;
+
+    // Use this for initialization
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        happySprite = Resources.Load<Sprite>("Sprites/DolphinHappy");
+        defaultSprite = Resources.Load<Sprite>("Sprites/DolphinDefault");
+    }
+
+    public void SetHappySprite()
+    {
+        spriteRenderer.sprite = happySprite;
+        Invoke("SetDefaultSprite", 1f);
+    }
+
+    public void SetDefaultSprite()
+    {
+        spriteRenderer.sprite = defaultSprite;
+    }
+
+}
